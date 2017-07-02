4ab76853fb8ca33fd4c9661f07773db93605b9f8
Bartłomiej Górski <fenix6462@gmail.com>
2017-07-02 22:12:34 +0200

    no message

---
+++ BakeCake/ViewModels/CreateRecipeViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BakeCake.ViewModels
+{
+    public class CreateRecipeViewModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public IEnumerable<RecipeProductViewModel> RecipeProducts { get; set; }
+        
+        public CreateRecipeViewModel()
+        {
+            RecipeProducts = new List<RecipeProductViewModel>();
+        }
+
+        public class RecipeProductViewModel
+        {
+            public string Name { get; set; }
+            public float Weight { get; set; }
+        }
+    }
+}
